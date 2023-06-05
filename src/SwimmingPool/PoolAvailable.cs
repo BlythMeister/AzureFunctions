3d5981db54814955fab7e94d046fd89c1e837464
Chris Blyth <blythmeister@gmail.com>
2023-06-05 07:08:55 +0100

    Attempted fixes and ability to view the raw json output from site

--- src/SwimmingPool/PoolAvailable.cs
+++ src/SwimmingPool/PoolAvailable.cs
@@ -8,7 +8,9 @@ using Shared;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Security.Authentication;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -55,18 +57,50 @@ namespace SwimmingPool
             }
         }
 
+        [FunctionName("RawJson")]
+        public static async Task<IActionResult> GetRawJson([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req, ILogger log)
+        {
+            log.LogInformation("Raw JSON Web function executed at: {Date}", DateTime.UtcNow);
+            try
+            {
+                var result = await GetRawData(log);
+                return new OkObjectResult(result);
+            }
+            catch (Exception e)
+            {
+                log.LogError(e, "Error running function");
+                return new ExceptionResult(e, true);
+            }
+        }
+
+        [FunctionName("AllPool")]
+        public static async Task<IActionResult> GetAllPool([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req, ILogger log)
+        {
+            log.LogInformation("Pool All Web function executed at: {Date}", DateTime.UtcNow);
+            try
+            {
+                var result = await GetPoolDateTimes(log);
+                return new OkObjectResult(result.Select(x => x.Available != "0" ? $"AVAILABLE - {x.Slot:f}" : $"NOT AVAILABLE - {x.Slot:f}"));
+            }
+            catch (Exception e)
+            {
+                log.LogError(e, "Error running function");
+                return new ExceptionResult(e, true);
+            }
+        }
+
         [FunctionName("AllPoolAvailable")]
         public static async Task<IActionResult> GetAllPoolAvailable([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req, ILogger log)
         {
             log.LogInformation("Pool All Available Web function executed at: {Date}", DateTime.UtcNow);
             try
             {
-                var result = (await GetGoodDates(log)).ToList();
+                var result = (await GetGoodBadDates(log)).ToList();
                 return new OkObjectResult(result.Select(x => x.Good ? $"GOOD - {x.Date:f}" : $"BAD - {x.Date:f}"));
             }
             catch (Exception e)
             {
-                log.LogError(e, "Error running check");
+                log.LogError(e, "Error running function");
                 return new ExceptionResult(e, true);
             }
         }
@@ -77,19 +111,19 @@ namespace SwimmingPool
             log.LogInformation("Pool Good Available Web function executed at: {Date}", DateTime.UtcNow);
             try
             {
-                var result = (await GetGoodDates(log)).ToList();
+                var result = (await GetGoodBadDates(log)).ToList();
                 return new OkObjectResult(result.Where(x => x.Good).Select(x => x.Date.ToString("f")));
             }
             catch (Exception e)
             {
-                log.LogError(e, "Error running check");
+                log.LogError(e, "Error running function");
                 return new ExceptionResult(e, true);
             }
         }
 
         private static async Task NotifyAvailableDates(ILogger log)
         {
-            var result = (await GetGoodDates(log)).ToList();
+            var result = (await GetGoodBadDates(log)).ToList();
 
             var sentDates = await Blobs.ReadAppDataBlob<List<DateTime>>("data.dat", log);
 
@@ -137,7 +171,7 @@ namespace SwimmingPool
             }
         }
 
-        private static async Task<IEnumerable<(DateTime Date, bool Good)>> GetGoodDates(ILogger log)
+        private static async Task<IEnumerable<(DateTime Date, bool Good)>> GetGoodBadDates(ILogger log)
         {
             var dates = await GetPoolAvailableDateTimes(log);
             var goodBadDates = new List<(DateTime Date, bool Good)>();
@@ -169,26 +203,38 @@ namespace SwimmingPool
             return goodBadDates;
         }
 
-        private static async Task<List<DateTime>> GetPoolAvailableDateTimes(ILogger log)
+        private static async Task<string> GetRawData(ILogger log)
         {
-            using var client = new HttpClient();
+            var handler = new HttpClientHandler
+            {
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
+                SslProtocols = SslProtocols.Tls13 | SslProtocols.Tls12 | SslProtocols.Tls11 | SslProtocols.Tls,
+            };
+
+            using var client = new HttpClient(handler)
+            {
+                DefaultRequestHeaders =
+                {
+                    {"Origin", Environment.GetEnvironmentVariable("POOL_URL_BASE") },
+                    {"Sec-Fetch-Site", "same-origin" },
+                    {"Sec-Fetch-Mode", "cors" },
+                    {"Sec-Fetch-Dest", "empty" },
+                    {"Referer", Environment.GetEnvironmentVariable("POOL_URL_BOOKINGS") },
+                    {"Cache-Control", "no-cache" },
+                    {"Accept", "*/*" },
+                    { "Accept-Encoding", "gzip, deflate, br" }
+                },
+                Timeout = TimeSpan.FromMinutes(10)
+            };
 
             var content = new FormUrlEncodedContent(new List<KeyValuePair<string, string>>
             {
-                new("action","dopbsp_calendar_schedule_get"),
-                new("dopbsp_frontend_ajax_request","true"),
-                new("id","1"),
-                new("year",Environment.GetEnvironmentVariable("POOL_CHECK_YEAR")),
-                new("firstYear","\"false\""),
+                new("action", "dopbsp_calendar_schedule_get"),
+                new("dopbsp_frontend_ajax_request", "true"),
+                new("id", "1"),
+                new("year", Environment.GetEnvironmentVariable("POOL_CHECK_YEAR")),
+                new("firstYear", "\"false\""),
             });
-
-            client.DefaultRequestHeaders.Add("Origin", Environment.GetEnvironmentVariable("POOL_URL_BASE"));
-            client.DefaultRequestHeaders.Add("Sec-Fetch-Site", "same-origin");
-            client.DefaultRequestHeaders.Add("Sec-Fetch-Mode", "cors");
-            client.DefaultRequestHeaders.Add("Sec-Fetch-Dest", "empty");
-            client.DefaultRequestHeaders.Add("Referer", Environment.GetEnvironmentVariable("POOL_URL_BOOKINGS"));
-
-            var sessions = new List<DateTime>();
 
             log.LogInformation("Calling for available");
             var result = await client.PostAsync(Environment.GetEnvironmentVariable("POOL_URL_POST"), content);
@@ -201,21 +247,32 @@ namespace SwimmingPool
                     throw new Exception("No content");
                 }
 
-                var obj = JObject.Parse(resultContent);
+                return resultContent;
+            }
 
-                foreach (var dateItem in obj.Children().Select(x => x.ToObject<JProperty>()))
+            throw new Exception($"Non Success Status Code {result.StatusCode} ({(int)result.StatusCode})");
+        }
+
+        private static async Task<List<(DateTime Slot, string Available)>> GetPoolDateTimes(ILogger log)
+        {
+            var sessions = new List<(DateTime Slot, string Available)>();
+
+            var resultContent = await GetRawData(log);
+
+            var obj = JObject.Parse(resultContent);
+
+            foreach (var dateItem in obj.Children().Select(x => x.ToObject<JProperty>()))
+            {
+                var date = dateItem.Name;
+                var dateO = JObject.Parse(dateItem.Value.ToObject<JValue>().Value.ToString());
+                foreach (var hourItem in dateO.Value<JObject>("hours").Children().Select(x => x.ToObject<JProperty>()))
                 {
-                    var date = dateItem.Name;
-                    var dateO = JObject.Parse(dateItem.Value.ToObject<JValue>().Value.ToString());
-                    foreach (var hourItem in dateO.Value<JObject>("hours").Children().Select(x => x.ToObject<JProperty>()))
+                    var hour = hourItem.Name;
+                    var hourO = JObject.Parse(hourItem.Value.ToString());
+                    var available = hourO.Value<string>("available");
+                    if (DateTime.TryParse($"{date}T{hour}", out var parsedDate) && parsedDate >= DateTime.UtcNow)
                     {
-                        var hour = hourItem.Name;
-                        var hourO = JObject.Parse(hourItem.Value.ToString());
-                        var available = hourO.Value<string>("available");
-                        if (available != "0" && DateTime.TryParse($"{date}T{hour}", out var parsedDate) && parsedDate >= DateTime.UtcNow)
-                        {
-                            sessions.Add(parsedDate);
-                        }
+                        sessions.Add((parsedDate, available));
                     }
                 }
             }
@@ -226,6 +283,13 @@ namespace SwimmingPool
             }
 
             return sessions;
+        }
+
+        private static async Task<List<DateTime>> GetPoolAvailableDateTimes(ILogger log)
+        {
+            var sessions = await GetPoolDateTimes(log);
+
+            return sessions.Where(x => x.Available != "0").Select(x => x.Slot).ToList();
         }
     }
 }
